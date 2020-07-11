617347434e749124697420977fe1ac66c186333e
alexander9306 <alexander9306@gmail.com>
2020-07-10 22:36:57 -0400

    trabajando en las entidades

---
+++ Sistema_Ventas/Sistema.Entidades/Almacen/ProductoFoto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Sistema.Entidades.Almacen
+{
+    class ProductoFoto
+    {
+        public int id { get; set; }
+        [Required]
+        public int producto_id { get; set; }
+        public byte[] foto { get; set; }
+    }
+}
---
+++ Sistema_Ventas/Sistema.Entidades/Ordenes/DetalleCarrito.cs
@@ -0,0 +1,20 @@
+﻿using Sistema.Entidades.Almacen;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Sistema.Entidades.Ordenes
+{
+    class DetalleCarrito
+    {
+        public int id { get; set; }
+        [Required]
+        public int carrito_id { get; set; }
+        [Required]
+        public int producto_id { get; set; }
+
+        public Carrito carrito { get; set; }
+        public Producto producto { get; set; }
+    }
+}
