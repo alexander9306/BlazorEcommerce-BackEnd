83bdb791bb7de6f26d2d0a0a24df65184d6e55b8
davidp45 <david.a.perez45@gmail.com>
2020-07-11 16:01:03 -0400

    commit 1

---
+++ Sistema_Ventas/Sistema.Entidades/Almacen/Producto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Sistema.Entidades.Almacen
+{
+    class Producto
+    {
+        public int id { get; set; }
+        [Required]
+        public int categoria_id { get; set; }
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "El nombre no debe de tener más de 50 caracteres, ni menos de 3 caracteres.")]
+        public string nombre { get; set; }
+        public decimal precio { get; set; }
+        public bool estado { get; set; }
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "La marca no debe de tener más de 50 caracteres, ni menos de 3 caracteres.")]
+        public string marca { get; set; }
+        [Required]
+        public int stock { get; set; }
+        [StringLength(256)]
+        public string descripcion { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+
+        public Categoria categoria { get; set; }
+        public ProductoFoto foto { get; set; }
+    }
+}
---
+++ Sistema_Ventas/Sistema.Entidades/Ordenes/Carrito.cs
@@ -0,0 +1,16 @@
+﻿using Sistema.Entidades.Almacen;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Sistema.Entidades.Ordenes
+{
+    class Carrito
+    {
+        public int id { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime update_at { get; set; }
+        public ICollection<Producto> productos { get; set; }
+    }
+}
