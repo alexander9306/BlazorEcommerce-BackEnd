75fa169d551c5478d75e591497df38bf9f9cf6fb
Yunior Mesa <yuniorm001@gmail.com>
2020-07-11 15:51:05 -0400

    Mi parte proyecto final

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-nuevo
-David listo!
+Aqui vamos a subir los cambios finales de la aplicacion los demas cambios se haran en las ramas de cada uno.
---
+++ Sistema_Ventas/Sistema.Entidades/Almacen/Categoria.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Sistema.Entidades.Almacen
+{
+    class Categoria
+    {
+        public int id { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "El nombre no debe de tener más de 50 caracteres, ni menos de 3 caracteres.")]
+        public string nombre { get; set; }
+        [StringLength(256)]
+        public string descripcion { get; set; }
+        public bool estado { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime update_at { get; set; }
+
+        public ICollection<Producto> productos { get; set; }
+    }
+}
