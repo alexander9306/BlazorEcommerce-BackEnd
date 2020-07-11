20c89c135a8cb92a745e73e6d395f003920eadb8
Yunior Mesa <yuniorm001@gmail.com>
2020-07-11 16:00:41 -0400

    Create Administrador.cs

---
+++ Sistema_Ventas/Sistema.Entidades/Usuario/Administrador.cs
@@ -0,0 +1,14 @@
+using Sistema.Entidades.Almacen;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Sistema.Entidades.Ordenes
+{
+    class Usuario
+    {
+        public int id { get; set; }
+        
+    }
+}
