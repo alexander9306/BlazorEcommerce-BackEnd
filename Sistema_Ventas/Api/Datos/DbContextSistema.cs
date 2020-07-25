549ca53195eb9518a7ad720bf3c1987aa4a77e41
alexander9306 <alexander9306@gmail.com>
2020-07-25 00:09:35 -0400

    trabajando con blazor

---
+++ Sistema_Ventas/Api/Datos/DbContextSistema.cs
@@ -0,0 +1,48 @@
+﻿namespace Api.Datos
+{
+    using Api.Datos.Mapping.Almacen;
+    using Api.Datos.Mapping.Usuario;
+    using Api.Entidades.Almacen;
+    using Api.Entidades.Ordenes;
+    using Api.Entidades.Usuario;
+    using Microsoft.EntityFrameworkCore;
+
+    public class DbContextSistema : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+
+        public DbSet<Producto> Productos { get; set; }
+
+        public DbSet<ProductoFoto> ProductoFotos { get; set; }
+
+        public DbSet<DetalleCarrito> DetalleCarritos { get; set; }
+
+        public DbSet<Carrito> Carritos { get; set; }
+
+        public DbSet<Orden> Ordenes { get; set; }
+
+        public DbSet<Pedido> Pedidos { get; set; }
+
+        public DbSet<Pago> Pagos { get; set; }
+
+        public DbSet<Administrador> Administradores { get; set; }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        public DbSet<Rol> Roles { get; set; }
+
+        //public DbSet<Slider> Sliders { get; set; }
+
+        public DbContextSistema(DbContextOptions<DbContextSistema> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new CategoriaMap());
+            modelBuilder.ApplyConfiguration(new ProductoMap());
+            modelBuilder.ApplyConfiguration(new AdministradorMap());
+            modelBuilder.ApplyConfiguration(new ClienteMap());
+            modelBuilder.ApplyConfiguration(new RolMap());
+        }
+    }
+}
---
+++ Sistema_Ventas/Api/Entidades/Ordenes/DetalleCarrito.cs
@@ -0,0 +1,19 @@
+﻿namespace Api.Entidades.Ordenes
+{
+    using Api.Entidades.Almacen;
+
+    public class DetalleCarrito
+    {
+        public int Id { get; set; }
+
+        public int CarritoId { get; set; }
+
+        public int ProductoId { get; set; }
+
+        public int Cantidad { get; set; }
+
+        public Producto Producto { get; set; }
+
+        public Carrito Carrito { get; set; }
+    }
+}
