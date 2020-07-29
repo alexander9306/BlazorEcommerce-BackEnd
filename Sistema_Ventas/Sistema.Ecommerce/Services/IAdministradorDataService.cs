7be82587adaa988516537fa48a6768dd6e375e38
Yunior Mesa <yuniorm001@gmail.com>
2020-07-29 12:43:34 -0400

    Update

---
+++ Sistema_Ventas/Sistema.Ecommerce/Services/IAdministradorDataService.cs
@@ -0,0 +1,15 @@
+﻿namespace Sistema.Ecommerce.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Sistema.Shared.Entidades.Usuario;
+
+    public interface IAdministradorDataService
+    {
+        Task<IEnumerable<Administrador>> Listar();
+
+        Task<Administrador> Mostrar(int id);
+
+    }
+}
---
+++ Sistema_Ventas/Sistema.Ecommerce/Services/IRolDataService.cs
@@ -0,0 +1,13 @@
+﻿namespace Sistema.Ecommerce.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Sistema.Shared.Entidades.Usuario;
+
+    public interface IRolDataService
+    {
+        Task<IEnumerable<Rol>> Listar();
+
+    }
+}
