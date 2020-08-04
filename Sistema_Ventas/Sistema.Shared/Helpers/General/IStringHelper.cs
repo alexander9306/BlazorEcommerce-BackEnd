b3fa5dfddaae1e70ae629b0520e0f92d9a080253
Yunior Mesa <yuniorm001@gmail.com>
2020-08-04 16:56:13 -0400

    u

--- Sistema_Ventas/Sistema.Shared/Helpers/General/IStringHelper.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sistema.Shared.Helpers.General
-{
-    public interface IStringHelper
-    {
-        public string GetDescripcion(string descripcion, int maxLength = 60);
-
-        public string GetEstado(bool estado);
-    }
-}
